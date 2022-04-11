0d0ff850b48999d0b86673a4e18d646c65284f29
AnkitSenapati <102649490+AnkitSenapati@users.noreply.github.com>
2022-04-11 16:00:31 +0530

    Add files via upload

---
+++ MaturityWeb/Controllers/ProjectsController.cs
@@ -0,0 +1,79 @@
+﻿using MaturityDAL.Models;
+using MaturityRepo;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MaturityWeb.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        MaturityAssessmentContext _context;
+        IProject obj;
+        public ProjectsController(IProject ob)
+        {
+            obj = ob;
+            _context = new MaturityAssessmentContext();
+        }
+        public IActionResult Index()
+        {
+            List<Project> result = obj.GetAllProj();
+            return View(result);
+        }
+
+        public IActionResult Create()
+        {
+            ViewData["FunctionId"] = new SelectList(_context.Functions, "FunctionId", "FunctionName");
+            return View();
+        }
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult Create(Project p)
+        {
+            obj.AddProj(p);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Details(int id)
+        {
+            Project p = obj.GetProjById(id);
+            return View(p);
+        }
+        public IActionResult Edit(int id)
+        {
+            ViewData["FunctionId"] = new SelectList(_context.Functions, "FunctionId", "FunctionName");
+            Project p = obj.GetProjById(id);
+            return View(p);
+        }
+        [HttpPost]
+        [ActionName("Edit")]
+        public IActionResult EditDetails(Project p)
+        {
+            obj.UpdateProj(p);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            Project p = obj.GetProjById(id);
+            return View(p);
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public IActionResult DeleteFunc(int id)
+        {
+            obj.DeleteProj(id);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Home()
+        {
+            List<Project> result = obj.GetAllProj();
+            return View(result);
+        }
+        public IActionResult UserHome()
+        {
+            return View();
+        }
+    }
+}
