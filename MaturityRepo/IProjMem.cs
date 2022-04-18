12dacf2b2d3e1e71588c3f3a18a2ab1881b779c5
AnkitSenapati <102649490+AnkitSenapati@users.noreply.github.com>
2022-04-18 17:06:30 +0530

    Add files via upload

---
+++ MaturityRepo/IProjMem.cs
@@ -0,0 +1,18 @@
+﻿using MaturityDAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaturityRepo
+{
+    public interface IProjMem
+    {
+        public List<ProjectMember> GetAllMem();
+        public ProjectMember GetMemById(int id);
+        public void AddMem(ProjectMember pm);
+        public void UpdateMem(ProjectMember pm);
+        public void DeleteMem(int id);
+    }
+}
