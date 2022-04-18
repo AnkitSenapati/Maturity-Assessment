5c359910170c0e33f5db78fa35ab9ca35bb3d718
AnkitSenapati <102649490+AnkitSenapati@users.noreply.github.com>
2022-04-18 17:01:55 +0530

    Add files via upload

---
+++ MaturityRepo/AnsRepo.cs
@@ -0,0 +1,52 @@
+﻿using MaturityDAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaturityRepo
+{
+    public class AnsRepo : IAns
+    {
+        private readonly MaturityAssessmentContext _context;
+        public AnsRepo(MaturityAssessmentContext context)
+        {
+            _context = context;
+        }
+        public void AddAns(Answer a)
+        {
+            _context.Answers.Include(i => i.Question);
+            _context.Answers.Add(a);
+            _context.SaveChanges();
+        }
+
+        public void DeleteAns(int id)
+        {
+            Answer ExistingAns = _context.Answers.Where(a => a.AnswerId == id).Include(c => c.Question).FirstOrDefault();
+            _context.Answers.Remove(ExistingAns);
+            _context.SaveChanges();
+        }
+
+        public List<Answer> GetAllAns()
+        {
+            List<Answer> ans = _context.Answers.Include(c => c.Question).ToList();
+
+            return ans;
+        }
+
+        public Answer GetAnsById(int id)
+        {
+            Answer a = _context.Answers.Where(a => a.AnswerId == id).Include(c => c.Question).FirstOrDefault();
+
+            return a;
+        }
+
+        public void UpdateAns(Answer a)
+        {
+            _context.Answers.Update(a);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ MaturityRepo/IAns.cs
@@ -0,0 +1,18 @@
+﻿using MaturityDAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaturityRepo
+{
+    public interface IAns
+    {
+        public List<Answer> GetAllAns();
+        public Answer GetAnsById(int id);
+        public void AddAns(Answer a);
+        public void UpdateAns(Answer a);
+        public void DeleteAns(int id);
+    }
+}
