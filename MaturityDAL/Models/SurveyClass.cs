ea7723f29fa494cbec141029270144caac296173
AnkitSenapati <102649490+AnkitSenapati@users.noreply.github.com>
2022-04-18 16:58:39 +0530

    Add files via upload

---
+++ MaturityDAL/Models/SurveyClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaturityDAL.Models
+{
+    public class SurveyClass
+    {
+        public Question qstndetails { get; set; }
+        public Answer ansdetails { get; set; }
+        public Function functiondetails { get; set; }
+    }
+}
---
+++ MaturityDAL/Models/UserSurveyClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaturityDAL.Models
+{
+    public class UserSurveyClass
+    {
+        public Project projectdetails { get; set; }
+        public UserSurvey userdetails { get; set; }
+        public ProjectMember memberdetails { get; set; }
+        public Function functiondetails { get; set; }
+
+        public Survey surveydetails { get; set; }
+
+    }
+}
